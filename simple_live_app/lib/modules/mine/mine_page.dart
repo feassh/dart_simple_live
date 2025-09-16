def4a0c7dec73ec2adb6fc0fa241ea28694c6a62
feassh <feassh@gmail.com>
2025-09-16 20:02:51 +0800

    1.0.10

--- simple_live_app/lib/modules/mine/mine_page.dart
+++ simple_live_app/lib/modules/mine/mine_page.dart
@@ -242,7 +242,6 @@ class MinePage extends StatelessWidget {
                   ),
                   onTap: Utils.showStatement,
                 ),
-                //
                 ListTile(
                   leading: const Icon(Remix.github_line),
                   title: const Text("开源主页"),
